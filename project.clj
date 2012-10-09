37d9dbb209bb5453b8a17d8a4743de6aa548cc4c
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-09 14:28:07 -0400

    created a lein project which will run a compojure app

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.6"]
+                 [compojure "1.1.3"]
+                 [enlive/enlive "1.0.1"]]
+  :main gratefulplace.server)
---
+++ src/gratefulplace/server.clj
@@ -0,0 +1,56 @@
+(ns gratefulplace.server
+  (:require compojure.route
+            compojure.handler)
+  (:use [ring.adapter.jetty :only (run-jetty)]
+        [ring.middleware.params :only (wrap-params)]
+        [compojure.core :only (GET PUT POST defroutes)]))
+
+(def ^:private counter (atom 0))
+(def ^:private mappings (ref {}))
+
+(defn url-for
+  [id]
+  (@mappings id))
+
+(defn shorten!
+  "Stores the given URL under a new unique identifier, or the given identifier
+if provided. Returns the identifier as a string.
+Modifies the global mapping accordingly."
+  ([url]
+     (let [id (swap! counter inc)
+           id (Long/toString id 36)]
+       (or (shorten! url id)
+           (recur url))))
+  ([url id]
+     (dosync
+      (when-not (@mappings id)
+        (alter mappings assoc id url)
+        id))))
+
+(defn retain
+  [& [url id :as args]]
+  (if-let [id (apply shorten! args)]
+    {:status 201
+     :headers {"Location" id}
+     :body (list "URL " url " assigned the short identifier " id)}
+    {:status 409 :body (format "Short URL %s is already taken" id)}))
+
+(defn redirect
+  [id]
+  (if-let [url (url-for id)]
+    (ring.util.response/redirect url)
+    {:status 404 :body (str "No such short URL: " id)}))
+
+(defroutes app*
+  (GET "/" request "Welcome!")
+  (PUT "/:id" [id url] (retain url id))
+  (POST "/" [url] (retain url))
+  (GET "/:id" [id] (redirect id))
+  (GET "/list/" [] (interpose "\n" (keys @mappings)))
+  (compojure.route/not-found "Sorry, there's nothing here."))
+
+(def app (compojure.handler/api app*))
+
+(defn -main
+  []
+  (run-jetty #'app {:port 8080 :join? false}))

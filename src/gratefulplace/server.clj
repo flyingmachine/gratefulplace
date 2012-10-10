bae07bbefa5011cbbc8f8bf9b24043dad407f14d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-10 10:11:39 -0400

    actually showing some data

--- src/gratefulplace/server.clj
+++ src/gratefulplace/server.clj
@@ -11,15 +11,36 @@
 ;;   [message]
 ;;   [:.footer] (h/content message))
 
+(def ^:private counter (atom 0))
+(def ^:private mappings (ref {}))
+(def posts
+  [{:author        "Terrence Blowfish"
+    :date          "Jan 03, 2012 9:53am"
+    :content       "Today I'm grateful for the trees turning colors"
+    :comment-count 10}
+   {:author        "Mickey Parkplace"
+    :date          "Jan 03, 2012 10:12am"
+    :content       "This morning I feel grateful for the food in my kitchen"
+    :comment-count 3}
+   {:author        "Jeremy McWilder"
+    :date          "Jan 03, 2012 10:14am"
+    :content       "I'm grateful for the cat in my lap"
+    :comment-count 0}])
 
+(defn comments [post]
+  (if (zero? (:comment-count post))
+    "Add a comment"
+    (str (:comment-count post) " comments")))
 ;; returns a seq of strings. to concat, use
 ;; (apply str (friends-list ...))
 (h/deftemplate home "gratefulplace/templates/index.html"
   []
-  [:.post :.content] (h/content "This is enlive content"))
-
-(def ^:private counter (atom 0))
-(def ^:private mappings (ref {}))
+  [:.post] (h/clone-for [post posts]
+                        [:.author]   (h/content (:author post))
+                        [:.date]     (h/content (:date post))
+                        [:.content]  (h/content (:content post))
+                        [:.comments] (h/content (comments post))
+                        (h/content "This is enlive content")))
 
 (defn url-for
   [id]

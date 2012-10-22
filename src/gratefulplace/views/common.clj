1498115dadeeee278b8b78e3019e60c6d2509d55
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-22 09:43:56 -0400

    move controllers common to views commongst

--- src/gratefulplace/views/common.clj
+++ src/gratefulplace/views/common.clj
@@ -6,3 +6,8 @@
 (h/defsnippet nav (str *template-dir* "index.html") [:nav]
   [logged-in]
   [:.auth] (h/content (if logged-in "Log Out" "Log In")))
+
+(h/deftemplate layout (str *template-dir* "index.html")
+  [html]
+  [:html] html
+  [:nav] (nav false))

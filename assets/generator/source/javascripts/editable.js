c591a6f5748d825349b1288dc6f10e429df4992d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-28 08:41:15 -0400

    added javascript for editing and upating

--- assets/generator/source/javascripts/editable.js
+++ assets/generator/source/javascripts/editable.js
@@ -1,3 +1,19 @@
-jQuery.each(["comment", "post"], function(i, val){
+var Editable = {
+  correspondingContent: function(el) {
+    return $(el).parents(".post, .comment").find(".content");
+  },
   
-})
+  setup: function() {
+    $(".edit a").click(function(ev) {
+      var node = this;
+      $.get($(node).attr("href"), function(data) {
+        Editable.correspondingContent(node).html(data);
+      });
+      return false;
+    });
+  }
+}
+
+$(Editable.setup)
+
+

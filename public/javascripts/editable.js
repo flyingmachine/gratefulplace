efffdfb367f63c1e03ebf1f5a96df4f0550d262d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-28 08:42:44 -0400

    generated assets

---
+++ public/javascripts/editable.js
@@ -0,0 +1,20 @@
+var Editable = {
+  correspondingContent: function(el) {
+    return $(el).parents(".post, .comment").find(".content");
+  },
+  
+  setup: function() {
+    $(".edit a").click(function(ev) {
+      var node = this;
+      $.get($(node).attr("href"), function(data) {
+        Editable.correspondingContent(node).html(data);
+      });
+      return false;
+    });
+  }
+}
+
+$(Editable.setup)
+
+
+;

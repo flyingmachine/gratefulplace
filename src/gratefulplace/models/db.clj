49f0f6f084746b1f1d45f277373f2a4589c0f972
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-11-02 18:28:41 -0400

    trying to get this working on heroku

--- src/gratefulplace/models/db.clj
+++ src/gratefulplace/models/db.clj
@@ -4,11 +4,10 @@
 ;; TODO figure out how to put this config in lein and read it from
 ;; there
 ;; google "lein full project map"
-(if (not (resolve 'db))
-  (defdb db (or (System/getenv "DATABASE_URL")
-                (postgres
-                 {:db "gratefulplace-development"
-                  :user "daniel"
-                  :password ""
-                  ;;OPTIONAL KEYS
-                  :host "localhost"}))))
+(defdb db (or (System/getenv "DATABASE_URL")
+              (postgres
+               {:db "gratefulplace-development"
+                :user "daniel"
+                :password ""
+                ;;OPTIONAL KEYS
+                :host "localhost"})))

917867fa89ec1763da729e29403fcad09311ee35
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-11-02 18:38:49 -0400

    another try at heroku db

--- src/gratefulplace/models/db.clj
+++ src/gratefulplace/models/db.clj
@@ -4,10 +4,20 @@
 ;; TODO figure out how to put this config in lein and read it from
 ;; there
 ;; google "lein full project map"
-(defdb db (or (System/getenv "DATABASE_URL")
-              (postgres
-               {:db "gratefulplace-development"
-                :user "daniel"
-                :password ""
-                ;;OPTIONAL KEYS
-                :host "localhost"})))
+(defdb db 
+  (if (System/getenv "DATABASE_URL")
+    (let [db-uri (java.net.URI. (System/getenv "DATABASE_URL"))
+          user-and-password (clojure.string/split (.getUserInfo db-uri) #":")]
+      {:classname "org.postgresql.Driver"
+       :subprotocol "postgresql"
+       :user (get user-and-password 0)
+       :password (get user-and-password 1) ; may be nil
+       :subname (if (= -1 (.getPort db-uri))
+                  (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
+                  (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))})
+    (postgres
+     {:db "gratefulplace-development"
+      :user "daniel"
+      :password ""
+      ;;OPTIONAL KEYS
+      :host "localhost"})))

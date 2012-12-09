093b33c10c8a4d914fc6adb1af262c9663ecd304
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-12-09 16:56:14 -0500

    create comment notification

--- src/gratefulplace/controllers/comments.clj
+++ src/gratefulplace/controllers/comments.clj
@@ -2,11 +2,14 @@
   (:require [ring.util.response :as res]
             [net.cgrand.enlive-html :as h]
             [gratefulplace.models.comment :as comment]
+            [gratefulplace.models.comment-notification :as notification]
+            [gratefulplace.models.post :as post]
             [gratefulplace.views.comments :as view]
             [cemerick.friend :as friend])
 
   (:use [gratefulplace.controllers.common :only (if-valid view)]
-        gratefulplace.controllers.common.content))
+        gratefulplace.controllers.common.content
+        gratefulplace.models.permissions))
 
 (defn create!
   [params]
@@ -14,8 +17,12 @@
    params comment/validations errors
    (let [comment (comment/create! (assoc params
                                     :user_id
-                                    (:id (friend/current-authentication))))]
+                                    (current-user-id)))
+         post-owner-id (:user_id (post/by-id (:post_id params)))]
      ;; TODO path stuff here
+     (if (not= (current-user-id) post-owner-id)
+       (notification/create! {:user_id post-owner-id
+                              :comment_id (:id comment)}))
      (res/redirect (str "/posts/" (:post_id params) "#comment-" (:id comment))))
    (res/redirect (str "/posts/" (:post_id params) "?blank-comment=true"))))
 
